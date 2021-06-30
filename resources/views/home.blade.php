5a91fe1eaf7a965b2183fa5d2cdea9135d0ee851
ecofab-pts <ecofab.pts@gmail.com>
2021-06-30 17:54:34 +0600

    Initial commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,38 @@
+@extends((Auth::user()->is_admin == 1) ? 'layouts.admin_app' : 'layouts.app')
+
+@section('content')
+    <div class="content-wrapper">
+        <!-- Content Header (Page header) -->
+        <div class="content-header">
+            <div class="container-fluid">
+                <div class="row mb-2">
+                    <div class="col-sm-6">
+                        <h1 class="m-0">Dashboard</h1>
+                    </div><!-- /.col -->
+                    <div class="col-sm-6">
+                        <ol class="breadcrumb float-sm-right">
+                            <li class="breadcrumb-item"><a href="{{ route('home') }}">Home</a></li>
+                            <li class="breadcrumb-item active">Dashboard</li>
+                        </ol>
+                    </div><!-- /.col -->
+                </div><!-- /.row -->
+            </div><!-- /.container-fluid -->
+        </div>
+        <!-- /.content-header -->
+
+        <!-- Main content -->
+        <div class="content">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-lg-12">
+                        <h3 class="btn btn-block btn-success btn-lg">Hey {{ (Auth::user()->is_admin == 1) ? 'ADMIN' : 'USER' }}, Welcome to Bill Tracker!</h3>
+                    </div>
+                    <!-- /.col-md-6 -->
+                </div>
+                <!-- /.row -->
+            </div>
+            <!-- /.container-fluid -->
+        </div>
+        <!-- /.content -->
+    </div>
+@endsection
