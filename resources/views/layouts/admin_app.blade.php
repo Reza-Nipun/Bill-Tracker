717e715afaf8f29918fa0aa83ed255b1c8ee2686
reza-nipun <nipunsarker56@gmail.com>
2021-07-10 11:09:57 +0600

    Update Favicon

--- resources/views/layouts/admin_app.blade.php
+++ resources/views/layouts/admin_app.blade.php
@@ -30,7 +30,12 @@
     <!-- dropzonejs -->
     <link rel="stylesheet" href="{{ asset('assets/plugins/dropzone/min/dropzone.min.css') }}">
 
-
+    <!-- Favicon Start -->
+    <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('assets/favicon/apple-touch-icon.png') }}">
+    <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('assets/favicon/favicon-32x32.png') }}">
+    <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('assets/favicon/favicon-16x16.png') }}">
+    <link rel="manifest" href="{{ asset('assets/favicon/site.webmanifest') }}">
+    <!-- Favicon End -->
 
     <style>
         .loader {
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,6 +13,13 @@
     <link rel="stylesheet" href="{{ asset('assets/plugins/icheck-bootstrap/icheck-bootstrap.min.css') }}">
     <!-- Theme style -->
     <link rel="stylesheet" href="{{ asset('assets/dist/css/adminlte.min.css') }}">
+
+    <!-- Favicon Start -->
+    <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('assets/favicon/apple-touch-icon.png') }}">
+    <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('assets/favicon/favicon-32x32.png') }}">
+    <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('assets/favicon/favicon-16x16.png') }}">
+    <link rel="manifest" href="{{ asset('assets/favicon/site.webmanifest') }}">
+    <!-- Favicon End -->
 </head>
 <body class="hold-transition sidebar-mini sidebar-collapse">
     <nav class="navbar navbar-light bg-light justify-content-between">
